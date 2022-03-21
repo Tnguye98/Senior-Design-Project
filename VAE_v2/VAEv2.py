01b0b9be28fe563b0852d64a69aec6ebd28e5a53
Tony Nguyen <69164218+Tnguye98@users.noreply.github.com>
2022-03-21 17:35:00 -0400

    Updated the file setup comment
    Made it a bit easier to read.

--- VAE_v2/VAEv2.py
+++ VAE_v2/VAEv2.py
@@ -1,17 +1,41 @@
-# This file is the primary training file for the VAE.
-# Training will fail unless a proper manifest is provided and the 'model' folder exists in the base directory
-#
-# File Usage:
-#	python vae.py [args]
-#
-#	Command Line Arguments:
-#	--load [int]	| Load checkpoint model and start at specified epoch
-#	-l [int]		| Alias for --load
-#	--arch			| Shown model architecture only, do not execute
-#	-a 				| Alias for --arch
-#
-# File Configuraion:
-#	To properly configure this file, you may edit the configuration and constants values
-#	These variables can be found under the following sections:
-#		CONFIGURATION VARIABLES 	- Line 70
-#		CONSTANTS CONFIGURATION 	- Line 90
+'''
+This file is the primary training file for the VAE. 
+Requires proper manifest and the 'model' folder
+
+File Usage:
+
+	python vae.py [args]
+
+	Command Line Args:
+		--load [int],     -l [int]			
+			Loads checkpoint model and start at a specified epcoh
+		
+		--arch, 		  -a
+			Shown model architecture only, do not execute
+
+File Configuration: 
+	To properly configure this file, you may edit the configuration
+	and constant values.
+		VARIABLES			- Line 70
+		CONSTANTS			- Line 90
+
+'''
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+import gc
+
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+
+
+
+
+
+
+
+
+
+
